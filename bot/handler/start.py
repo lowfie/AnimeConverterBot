64f97bde026f8bd7cf14036ee9a9c2aae5ef9c27
Andrew <vesell4123@mail.ru>
2023-03-31 11:36:19 +0300

    adding photo to start command

--- bot/handler/start.py
+++ bot/handler/start.py
@@ -2,14 +2,16 @@ from aiogram import types
 
 from bot.base import bot, dp
 from bot.utils import anti_flood
+from config import START_FILE_ID
 
 
 @dp.message_handler(commands="start")
 @dp.throttled(anti_flood, rate=1)
 async def start(message: types.Message):
-    await bot.send_message(
+    await bot.send_photo(
         message.chat.id,
-        f"👋 Привет, {message.chat.first_name}\n\n"
+        photo=START_FILE_ID,
+        caption=f"👋 Привет, {message.chat.first_name}\n\n"
         "📸 Отправь сюда <b>любое фото</b>, и я преобразую его в аниме стиль с помощью нейросетей\n\n"
         "🧸 Также можешь добавить бота в чат и развлекаться вместе с друзьями👇",
         parse_mode="HTML"
--- config.py
+++ config.py
@@ -5,6 +5,8 @@ env.read_env()
 
 TOKEN = env.str("TOKEN")
 
+START_FILE_ID = env.str("START_FILE_ID")
+
 JOIN_CHAT_TEXT = env.str("JOIN_CHAT_TEXT")
 JOIN_CHAT_FILE_ID = env.str("JOIN_CHAT_ANIMATION")
 
