d6673efc2a5f5709f3dbda5bfe04370d23278efa
Andrew <vesell4123@mail.ru>
2023-03-31 11:17:18 +0300

    replace message to animation

--- bot/handler/chat_join.py
+++ bot/handler/chat_join.py
@@ -1,13 +1,15 @@
-from aiogram import types, filters
+from aiogram import types
 
 from bot.base import bot, dp
-from config import JOIN_CHAT_TEXT
+from config import JOIN_CHAT_TEXT, JOIN_CHAT_FILE_ID
 
 
 @dp.chat_join_request_handler()
 async def approve_member(chat_join: types.ChatJoinRequest):
-    await bot.send_message(
-        chat_id=chat_join.from_user.id,
-        text=JOIN_CHAT_TEXT
+    await bot.send_animation(
+        chat_join.from_user.id,
+        animation=JOIN_CHAT_FILE_ID,
+        caption=JOIN_CHAT_TEXT,
+        parse_mode="HTML"
     )
     await chat_join.approve()
