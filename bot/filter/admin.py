c8c6262d5ed367f6fc25838916a7af6e7b40f73d
Andrew <vesell4123@mail.ru>
2023-03-31 13:00:05 +0300

    create admin filter

---
+++ bot/filter/admin.py
@@ -0,0 +1,10 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Filter
+
+from config import ADMINS
+
+
+class IsAdmin(Filter):
+    async def check(self, message: types.Message) -> bool:
+        user = message.from_user.id
+        return user in ADMINS
