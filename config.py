c984e4e926865bce0f28a705f3803fabcfcb0983
Andrew <vesell4123@mail.ru>
2023-03-31 13:00:54 +0300

    create list variable

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ START_FILE_ID = env.str("START_FILE_ID")
 JOIN_CHAT_TEXT = env.str("JOIN_CHAT_TEXT")
 JOIN_CHAT_FILE_ID = env.str("JOIN_CHAT_ANIMATION")
 
-chats = [
-    # format {"title": "", "chat_id": , "invited_link": ""}
+ADMINS = [
+    813415126,
 ]
 
