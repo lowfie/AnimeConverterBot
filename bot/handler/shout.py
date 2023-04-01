f617006ab4d05824b32f8f23ad7c93205bc5b16f
Andrew <vesell4123@mail.ru>
2023-04-01 14:16:26 +0300

    bugfix: rename var's

--- bot/handler/shout.py
+++ bot/handler/shout.py
@@ -1,11 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from sqlalchemy import select
 
 from bot.base import bot, dp
 from bot.filter.admin import IsAdmin
+from bot.utils import send_message_media_types
 from database.base import session
 from database.models import User
 
@@ -33,41 +33,30 @@ async def process_text(message: types.Message, state: FSMContext):
         data["text"] = message.parse_entities()
         await Form.next()
         await message.reply(
-            "Отправьте медиафайл и дождитесь отправки\n"
-            "Вам придёт уведомление, когда все сообщения будут отправлены\n"
-            "И нажмите кнопку отмена, либо нажмите сразу, но отправка прекратиться",
+            "Отправьте медиафайл и дождитесь отправки",
             reply_markup=markup
         )
 
 
 @dp.message_handler(content_types=["photo", "video", "animation"],  state=Form.media)
-async def process_text(message: types.Message, state: FSMContext):
+async def process_media(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.content_type == "photo":
-            for user_id in users:
-                chat = user_id[0]
-                await bot.send_photo(
-                    chat_id=chat,
-                    photo=message.photo[0].file_id,
-                    caption=data["text"],
-                    parse_mode="HTML"
-                )
+            file_id=message.photo[0].file_id
         elif message.content_type == "video":
-            for user_id in users:
-                chat = user_id[0]
-                await bot.send_video(
-                    chat_id=chat,
-                    video=message.video.file_id,
-                    caption=data["text"],
-                    parse_mode="HTML"
-                )
+            file_id=message.video.file_id
         elif message.content_type == "animation":
-            for user_id in users:
-                chat = user_id[0]
-                await bot.send_animation(
-                    chat_id=chat,
-                    animation=message.animation.file_id,
-                    caption=data["text"],
-                    parse_mode="HTML"
-                )
+            file_id=message.animation.file_id
+
+        for user_id in users:
+            chat = user_id[0]
+            await send_message_media_types(
+                bot=bot,
+                content_type=message.content_type,
+                chat_id=chat,
+                text=data["text"],
+                file_id=file_id
+            )
+
     await bot.send_message(message.from_user.id, "Все сообщения были успешно отправлены")
+    await state.finish()
