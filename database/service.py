783869bbc41590ac1d4ed3490886306b55933a01
Andrew <vesell4123@mail.ru>
2023-04-01 13:52:47 +0300

    create init_join_chat_message and update_join_chat_message

--- database/service.py
+++ database/service.py
@@ -1,6 +1,6 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
-from database.models import User
+from database.models import User, JoinChatMessage
 from database.base import session
 
 
@@ -15,3 +15,41 @@ async def add_user(tg_id):
     except Exception as err:
         print("Ошибка добавления пользователей", err)
         session.rollback()
+
+
+async def init_join_chat_message(text_type: str):
+    is_text_type = (session.execute(
+            select(JoinChatMessage.text_type)
+            .where(JoinChatMessage.text_type.__eq__(text_type))
+        )).first()
+
+    if not is_text_type:
+        join_chat = JoinChatMessage(text_type=text_type)
+        session.add(join_chat)
+        session.commit()
+
+
+async def update_join_chat_message(text_type: str, content_type: str, text: str, file_id: str | None):
+    session.execute(
+        update(JoinChatMessage)
+        .values(
+            content_type=content_type,
+            text=text,
+            file_id=file_id
+        )
+        .where(JoinChatMessage.text_type.__eq__(text_type))
+    )
+    session.commit()
+
+
+async def select_join_chat_message(text_type: str):
+    join_chat_message = (session.execute(
+        select(
+            JoinChatMessage.content_type,
+            JoinChatMessage.text,
+            JoinChatMessage.file_id
+        )
+        .where(JoinChatMessage.text_type.__eq__(text_type))
+    )).first()
+    return join_chat_message
+
