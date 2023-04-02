6d67886d0ed26c85d9cd276777b82b30b99ea8d1
Andrew <vesell4123@mail.ru>
2023-04-02 03:55:15 +0300

    change database response to service func

--- bot/handler/bot_blocked.py
+++ bot/handler/bot_blocked.py
@@ -1,22 +1,11 @@
 from aiogram import types
-from sqlalchemy import update
 
-from database.models import User
-from database.base import session
+from database.service import set_life_user
 
 
 async def user_blocked_bot(my_chat_member: types.ChatMemberUpdated):
     if my_chat_member.new_chat_member.status == types.ChatMemberStatus.KICKED:
-        session.execute(
-            update(User)
-            .values(is_life=False)
-            .where(User.tg_id.__eq__(my_chat_member.from_user.id))
-        )
+        await set_life_user(life_status=False, tg_id=my_chat_member.from_user.id)
 
     if my_chat_member.new_chat_member.status == types.ChatMemberStatus.MEMBER:
-        session.execute(
-            update(User)
-            .values(is_life=True)
-            .where(User.tg_id.__eq__(my_chat_member.from_user.id))
-        )
-    session.commit()
+        await set_life_user(life_status=True, tg_id=my_chat_member.from_user.id)
