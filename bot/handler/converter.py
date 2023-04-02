1a78623bab7706077efc63a3cb719ac4c76d5e54
Andrew <vesell4123@mail.ru>
2023-04-02 04:25:34 +0300

    advertising message after photo processing

--- bot/handler/converter.py
+++ bot/handler/converter.py
@@ -1,9 +1,69 @@
 import base64
+import asyncio
 import requests
 
-from settings.config import TOKEN
+from loguru import logger
+from datetime import datetime, timedelta
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
 from bot.base import dp, bot
 from bot.utils import anti_flood, get_ai_image
+from bot.utils import send_message_media_types
+from settings.config import TOKEN
+from database.service import (
+    init_chat_message,
+    update_chat_message,
+    select_chat_message
+)
+
+
+class FormAfterPhoto(StatesGroup):
+    message = State()
+
+
+async def text_after_photo(message: types.Message, state: FSMContext = None):
+    await init_chat_message("after_photo")
+    await FormAfterPhoto.message.set()
+    async with state.proxy() as data:
+        data["text_type"] = "after_photo"
+    await message.reply("Отправь сообщение уведомления после отправки ботом фото (с медиафайлом)")
+
+
+async def process_media_after_photo(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        if message.content_type == "text":
+            await update_chat_message(
+                text_type=data["text_type"],
+                content_type=message.content_type,
+                text=message.parse_entities(),
+                file_id=None
+            )
+        elif message.content_type == "photo":
+            await update_chat_message(
+                text_type=data["text_type"],
+                content_type=message.content_type,
+                text=message.parse_entities(),
+                file_id=message.photo[0].file_id
+            )
+        elif message.content_type == "video":
+            await update_chat_message(
+                text_type=data["text_type"],
+                content_type=message.content_type,
+                text=message.parse_entities(),
+                file_id=message.video.file_id
+            )
+        elif message.content_type == "animation":
+            await update_chat_message(
+                text_type=data["text_type"],
+                content_type=message.content_type,
+                text=message.parse_entities(),
+                file_id=message.animation.file_id
+            )
+    await message.reply("Данные были обновлены")
+    await state.finish()
 
 
 @dp.throttled(anti_flood, rate=0.5)
@@ -30,5 +90,20 @@ async def send_anime_photo(message):
     try:
         ai_image = get_ai_image(base64_image_string)["media_info_list"][0]["media_data"]
         await bot.send_photo(message.from_user.id, ai_image, caption=f"@{tag}")
-    except:
+
+        after_photo_content_type, after_photo_text, after_photo_file_id = await select_chat_message("after_photo")
+        send_date = datetime.now() + timedelta(seconds=10)
+        while datetime.now() <= send_date:
+            await asyncio.sleep(1)
+            if datetime.now() >= send_date:
+                await send_message_media_types(
+                    bot=bot,
+                    content_type=after_photo_content_type,
+                    chat_id=message.from_user.id,
+                    text=after_photo_text,
+                    file_id=after_photo_file_id
+                )
+                break
+    except Exception as _ex:
+        logger.warning("Ошибка нейросети", _ex)
         await bot.send_message(message.from_user.id, "🚨 Произошла ошибка, попробуйте еще раз")
