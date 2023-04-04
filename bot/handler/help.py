ea69ab1319e4778a4f8aea0b6ab82f09929d2125
Andrew <vesell4123@mail.ru>
2023-04-05 01:11:46 +0300

    add new command - admin help

---
+++ bot/handler/help.py
@@ -0,0 +1,19 @@
+from aiogram import types
+
+from bot.base import bot, dp
+from bot.utils import anti_flood
+
+
+@dp.throttled(anti_flood, rate=1)
+async def admin_help(message: types.Message):
+    await bot.send_message(
+        message.chat.id,
+        text="<b>Помощь</b>\n\n"
+             "<code>/tjc /text_join_chat</code> - <b>уведомление при входе в канал</b>\n"
+             "<code>/taj /text_after_join</code> - <b>уведомление через 15 минут после входа в канал</b>\n"
+             "<code>/tap /text_after_photo</code> - <b>уведомление после обработки фото</b>\n"
+             "<code>/shout</code> - <b>рассылка сообщения</b>\n"
+             "<code>/stats</code> - <b>статистика активных пользователей</b>\n",
+        parse_mode="HTML",
+        reply_markup=types.ReplyKeyboardRemove()
+    )
