6c2d3f2a5b43d4a2a5085902163863d95ee1c6f1
Andrew <vesell4123@mail.ru>
2023-04-04 17:04:11 +0300

    register pin_button state

--- bot/register.py
+++ bot/register.py
@@ -13,7 +13,13 @@ async def register_bot_handlers(dispatcher: Dispatcher):
     from bot.handler.bot_blocked import user_blocked_bot
     from bot.handler.shout import cmd_shout, process_text, process_media, FormShout
     from bot.handler.subscriber import add_subscribe, remove_subscribe, get_all_subscriber
-    from bot.handler.converter import send_anime_photo, text_after_photo, process_media_after_photo, FormAfterPhoto
+    from bot.handler.converter import (
+        text_after_photo,
+        process_pin_button,
+        process_media_after_photo,
+        FormAfterPhoto,
+        send_anime_photo
+    )
     from bot.handler.chat_join import (
         text_join_to_chat,
         text_after_join_chat,
@@ -22,19 +28,25 @@ async def register_bot_handlers(dispatcher: Dispatcher):
         FormJoinMessage
     )
 
+    # /start - starting bot
     dispatcher.register_message_handler(start, commands="start")
 
+    # /stats - get stat of users life
     dispatcher.register_message_handler(stats_of_users, IsAdmin(), commands="stats")
 
+    # handler conversation and send photo
     dispatcher.register_message_handler(send_anime_photo, IsSubscriber(), content_types=["photo"])
 
+    # /cancel - cancel state machine
     dispatcher.register_message_handler(cancel_handler, state="*", commands="cancel")
     dispatcher.register_message_handler(cancel_handler, Text(equals="вернуться к боту", ignore_case=True), state="*")
 
+    # CRUD command for adding button sponsors
     dispatcher.register_message_handler(add_subscribe, IsAdmin(), commands="add_sub")
     dispatcher.register_message_handler(remove_subscribe, IsAdmin(), commands="remove_sub")
     dispatcher.register_message_handler(get_all_subscriber, IsAdmin(), commands="subs")
 
+    # mailing to all users /shout command
     dispatcher.register_message_handler(cmd_shout, IsAdmin(), commands="shout")
     dispatcher.register_message_handler(process_text, content_types=["text"], state=FormShout.text)
     dispatcher.register_message_handler(
@@ -43,6 +55,7 @@ async def register_bot_handlers(dispatcher: Dispatcher):
         state=FormShout.media
     )
 
+    # /text_join_chat , /text_after_chat command
     dispatcher.register_message_handler(text_join_to_chat, IsAdmin(), commands=["text_join_chat", "tjc"])
     dispatcher.register_message_handler(text_after_join_chat, IsAdmin(), commands=["text_after_join", "taj"])
     dispatcher.register_message_handler(
@@ -51,7 +64,13 @@ async def register_bot_handlers(dispatcher: Dispatcher):
         state=FormJoinMessage.message
     )
 
+    # /text_after_photo command
     dispatcher.register_message_handler(text_after_photo, IsAdmin(), commands=["text_after_photo", "tap"])
+    dispatcher.register_message_handler(
+        process_pin_button,
+        content_types=["text"],
+        state=FormAfterPhoto.button
+    )
     dispatcher.register_message_handler(
         process_media_after_photo,
         content_types=["text", "photo", "video", "animation"],
