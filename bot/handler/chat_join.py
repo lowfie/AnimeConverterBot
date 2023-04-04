aabc83ef9a18439e49ffdb346b812ccf32639810
Andrew <vesell4123@mail.ru>
2023-04-04 19:22:11 +0300

    add button to text's

--- bot/handler/chat_join.py
+++ bot/handler/chat_join.py
@@ -6,7 +6,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from datetime import datetime, timedelta
 
 from bot.base import bot
-from bot.utils import send_message_media_types
+from bot.utils import send_message_media_types, validation_button
 from database.service import (
     init_chat_message,
     update_chat_message,
@@ -16,23 +16,37 @@ from database.service import (
 
 
 class FormJoinMessage(StatesGroup):
+    button = State()
     message = State()
 
 
 async def text_join_to_chat(message: types.Message, state: FSMContext = None):
     await init_chat_message("join_chat")
-    await FormJoinMessage.message.set()
+    await FormJoinMessage.button.set()
     async with state.proxy() as data:
         data["text_type"] = "join_chat"
-    await message.reply("Отправь сообщение уведомления при входе в группу (с медиафайлом)")
+    await message.reply("Введите через пробел сначала текст, после ссылку кнопки для добавления")
 
 
 async def text_after_join_chat(message: types.Message, state: FSMContext = None):
     await init_chat_message("after_join")
-    await FormJoinMessage.message.set()
+    await FormJoinMessage.button.set()
     async with state.proxy() as data:
         data["text_type"] = "after_join"
-    await message.reply("Отправь сообщение уведомления при входе после 15 минут (с медиафайлом)")
+    await message.reply("Введите через пробел сначала текст, после ссылку кнопки для добавления")
+
+
+async def process_join_chat_pin_button(message: types.Message, state: FSMContext):
+    button = validation_button(message)
+    async with state.proxy() as data:
+        data["btn_text"], data["btn_url"] = button
+    await message.answer("Кнопка не была добавлена" if None in button else "Кнопка была добавлена")
+
+    await FormJoinMessage.next()
+    if data["text_type"] == "join_chat":
+        await message.reply("Отправь сообщение уведомления при входе в группу (с медиафайлом)")
+    elif data["text_type"] == "after_join":
+        await message.reply("Отправь сообщение уведомления при входе после 15 минут (с медиафайлом)")
 
 
 async def process_media_join_chat(message: types.Message, state: FSMContext):
@@ -42,44 +56,54 @@ async def process_media_join_chat(message: types.Message, state: FSMContext):
                 text_type=data["text_type"],
                 content_type=message.content_type,
                 text=message.parse_entities(),
-                file_id=None
+                file_id=None,
+                button_text=data["btn_text"],
+                button_url=data["btn_url"]
             )
         elif message.content_type == "photo":
             await update_chat_message(
                 text_type=data["text_type"],
                 content_type=message.content_type,
                 text=message.parse_entities(),
-                file_id=message.photo[0].file_id
+                file_id=message.photo[0].file_id,
+                button_text=data["btn_text"],
+                button_url=data["btn_url"]
             )
         elif message.content_type == "video":
             await update_chat_message(
                 text_type=data["text_type"],
                 content_type=message.content_type,
                 text=message.parse_entities(),
-                file_id=message.video.file_id
+                file_id=message.video.file_id,
+                button_text=data["btn_text"],
+                button_url=data["btn_url"]
             )
         elif message.content_type == "animation":
             await update_chat_message(
                 text_type=data["text_type"],
                 content_type=message.content_type,
                 text=message.parse_entities(),
-                file_id=message.animation.file_id
+                file_id=message.animation.file_id,
+                button_text=data["btn_text"],
+                button_url=data["btn_url"]
             )
     await message.reply("Данные были обновлены")
     await state.finish()
 
 
 async def approve_member(chat_join: types.ChatJoinRequest):
-    join_msg_content_type, join_msg_text, join_msg_file_id = await select_chat_message("join_chat")
-    after_msg_content_type, after_msg_text, after_msg_file_id = await select_chat_message("after_join")
+    join_content_type, join_text, join_file_id, join_btn_text, join_btn_url = await select_chat_message("join_chat")
+    after_content_type, after_text, after_file_id, after_btn_text, after_btn_url = await select_chat_message("after_join")
 
     await add_user(chat_join.from_user.id)
     await send_message_media_types(
         bot=bot,
-        content_type=join_msg_content_type,
+        content_type=join_content_type,
         chat_id=chat_join.from_user.id,
-        text=join_msg_text,
-        file_id=join_msg_file_id
+        text=join_text,
+        file_id=join_file_id,
+        button_text=join_btn_text,
+        button_url=join_btn_url
     )
     await chat_join.approve()
 
@@ -89,9 +113,11 @@ async def approve_member(chat_join: types.ChatJoinRequest):
         if datetime.now() >= send_date:
             await send_message_media_types(
                 bot=bot,
-                content_type=after_msg_content_type,
+                content_type=after_content_type,
                 chat_id=chat_join.from_user.id,
-                text=after_msg_text,
-                file_id=after_msg_file_id
+                text=after_text,
+                file_id=after_file_id,
+                button_text=after_btn_text,
+                button_url=after_btn_url
             )
             break
